ad8566d83605ae6c9d952e109e8d855308deb77c
Olvenn <olga-vencel@yandex.ru>
2023-12-12 15:59:53 +0300

    End hw-7.

--- HW/HW-7-Recursion/Program.cs
+++ HW/HW-7-Recursion/Program.cs
@@ -1,15 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
 // Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N (включительно). Использовать рекурсию, не использовать циклы.
 
 void printNums(int M, int N) {
-  if (N < 0 & M < 0) {
-    System.Console.WriteLine("Numb must be graiter then 0.");
-  }
-  if (N == M) {
-    System.Console.WriteLine("The numbers are equal.");
-    return;
+   if (N <= 0 & M <= 0) {
+    System.Console.WriteLine("Numbs must be graiter then 0.");
   }
 
   if (N < M) {
@@ -18,27 +13,81 @@ void printNums(int M, int N) {
     N = temp;
   }
 
-  // System.Console.WriteLine(M + " " + N);
-  // System.Console.WriteLine();
+  if (M < 1) {
+    M = 1;
+  }
 
-  if(N >= M & N > 0) {
-    printNums(M, N - 1);
-    Console.Write(N + " ");
-
+  if(N > M & M > 0) {
+    Console.Write(M + " " + N);
+    printNums(M + 1, N);
+  } else if (N == M)
+  {
+    Console.Write(M + " ");
   }
 }
-// 2 6 2 5 2 4 2 3
 
-printNums(10, 10);
+printNums(0, 0);
 
-// void NaturNum(int n){
-//     if (n > 0) {
-//         NaturNum(n-1);
-//         Console.Write(n + " ");
-//     }
-//     else if(n<0){
-//         Console.WriteLine("Numb must be graiter then 0");
-//     }    
-// }
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-// NaturNum(-5);
+int Ackermann(int m, int n) {
+  if(m < 0 | n < 0) {
+    return 0;
+  }
+  if (m == 0) {
+    return n + 1;
+  } else if ((m != 0) && (n == 0)) {
+      return Ackermann(m - 1, 1);
+      // 3 - 1, 1
+    } else {
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+    
+    }
+}
+
+System.Console.WriteLine(Ackermann(3, 2)); // 29
+System.Console.WriteLine();
+
+// в Презентации m = 2, n = 3 - A(m, n) = 29 
+
+int A(int m, int n)
+{
+  if (n == 0)
+    return m + 1;
+  else 
+    if ((n!= 0) && (m == 0))
+      return A(1, n - 1);
+
+    else
+      return A(A(m - 1, n), n - 1);
+  return 10;
+}
+System.Console.WriteLine(A(2, 3));
+
+// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] CreateArray(int max, int min, int size)
+{
+  int[] array = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = new Random().Next(min, max + 1);
+  }
+  return array;
+}
+
+System.Console.WriteLine();
+
+int[] incomeArray = CreateArray(10, 1, 5);
+
+Console.WriteLine(string.Join(" ", incomeArray));
+
+void PrintArray(int[] incomeArray) {
+
+  if (incomeArray.Length > 0) {
+    Console.Write(incomeArray[incomeArray.Length - 1] + " ");
+    PrintArray(incomeArray.SkipLast(1).ToArray());
+  }
+}
+
+PrintArray(incomeArray);
